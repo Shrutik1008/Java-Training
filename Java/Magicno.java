d707c11dbf134078e9af2585240ae26f2bfa9cc6
Shrutik Vankundre <shrutikvankundre1008@gmail.com>
2025-07-17 09:52:43 +0530

    Update Magicno.java

--- Java/Magicno.java
+++ Java/Magicno.java
@@ -1,19 +1,21 @@
 public class Magicno {
     public static void main(String[] args) {
-        int number = 10;
-        int sum = 0;
-        int temp = number;
+        int number = 28;
+        int sum = number;
 
-        
-        while (temp > 0) {
-            sum += temp % 10;
-            temp /= 10;
+        while (sum > 9) {
+            int temp = sum;
+            sum = 0;
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
         }
-        
+
         if (sum == 1) {
             System.out.println(number + " is a Magic number.");
         } else {
             System.out.println(number + " is not a Magic number.");
         }
-}
+    }
 }
